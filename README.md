73b7f2b633450e183125fdf2a09900578b0d3522
Kjell <kjellapps@gmail.com>
2021-08-29 16:34:41 -0700

    Plugin works again

--- README.md
+++ README.md
@@ -1,16 +1,67 @@
-## Rollover Daily Todos
+# Rollover Daily Todos
 
-This Obsidian plugin will rollover any incomplete todo items from your last daily note whenever you create a new daily note.
+This Obsidian plugin will rollover any incomplete todo items from the previous daily note (could be yesterday, or a week ago) to today. This is triggered automatically when a new daily note is created via the internal `Daily notes` plugin, or the `Periodic Notes` plugin., It can also be run as a command from the Command Palette.
 
 ![A demo of the plugin working](./demo.gif)
 
 ## Usage
 
-If you use the Core Daily Note plugin, whenever you create a new daily note, any unchecked checkboxes from your last daily note will be brought over to your new daily note.
+### 1. New Daily Note
 
-The plugin will look at the settings for the Daily Notes plugin to see where your daily notes are stored.
+Just create a new daily note using the `Daily notes` or `Periodic Notes` plugin. The previous day's incomplete todos will be rolled over to today's daily note.
 
-If you use a template for your daily note you can go to the plugin settings to select a header you would like the checkboxes to be added under.
+### 2. Command: Rollover Todos Now
+
+You can also open your command palette (CMD+P on macOS) and start typing `roll` to find this command. No matter where you are in Obsidian, the previous day's todos will get rolled forward.
+
+## Requirements
+
+- [x] You must have either:
+  1. `Daily notes` plugin installed *or*
+  2. `Periodic Notes` plugin installed AND the **Daily Notes** setting toggled on
+- [x] A Note folder set in one of these plugins. Inside it you must have:
+  1. 2 or more notes
+  2. All notes must be named in the format you use for daily notes (for example `2021-08-29` for `YYYY-MM-DD` )
+
+## Settings
+
+### 1. Template Heading
+
+If you chose a template file to use for new daily notes in `Daily notes > Settings` or `Periodic Notes > Settings`, you will be able to choose a heading for incomplete notes to roll into. Note that incomplete todos are taken from the entire file, regardless of what heading they are under. And they are all rolled into today's daily note, right under the heading of choice.
+
+If you leave this field as blank, or select `None`, then incomplete todos will be rolled onto the end of today's note (for new notes with no template, the end is the beginning of the note).
+
+### 2. Delete todos from previous day
+
+By default, this plugin will actually make a copy of incomplete todos. So if you forgot to wash your dog yesterday, and didn't check it off, then you will have an incomplete checkmark on yesterday's daily note, and a new incomplete checkmark will be rolled into today's daily note.
+
+Toggling this setting on will remove incomplete todos from the previous daily note once today's daily note has a copy of them.
+
+### 3. Remove empty todos in rollover
+
+By default, this plugin will roll over anything that has a checkbox, whether it has content or not. Toggling this setting on will ignore empty todos. If you have **#2** from above toggled on, it will also delete empty todos.
+
+## Bugs
+
+- Sometimes, if you trigger the `rollover` function too quickly, it will read the state of a file before the new data was saved to disk. For example, if you add a new incomplete todo to yesterday's daily note, and then quickly run the `Rollover Todos Now` command, it may grab the state of the file a second or two before you ran the command.
+
+For example (no template heading, empty todos toggled on):
+```markdown
+You type in:
+
+- [x] Do the dishes
+- [ ] Take out the trash
+
+And then you run the Rollover Todos Now command. Today's daily note might look like:
+
+- [ ] Take out the t
+
+And the previous day might look like
+
+- [x] Do the dishesrash
+```
+
+To fix this, avoid using the `Rollover Todos Now` or making new daily notes super quickly after messing with previous day's notes. Simply waiting 2-5 seconds before the rollover function is called should make this problem obsolete. I'm sure there is a solution to this, but for now, it'll just be one of the developers' todos.
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 import { Notice, Plugin, Setting, PluginSettingTab } from 'obsidian';
+import { getDailyNoteSettings, getAllDailyNotes, getDailyNote } from 'obsidian-daily-notes-interface'
 
 const MAX_TIME_SINCE_CREATION = 5000; // 5 seconds
 
 export default class RolloverTodosPlugin extends Plugin {
-	checkDailyNotesEnabled() {
-        const plugin = this.app.internalPlugins.plugins['daily-notes'];
-        return plugin && plugin.enabled;
+
+	async loadSettings() {
+		const DEFAULT_SETTINGS = {
+			templateHeading: 'none',
+			deleteOnComplete: false,
+			removeEmptyTodos: false,
+		}
+	  this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
-	getDailyNotesDirectory() {
-		if (this.dailyNotesDirectory != null) {
-			return this.dailyNotesDirectory;
-		}
+	async saveSettings() {
+	  await this.saveData(this.settings)
+	}
 
-		this.dailyNotesDirectory = this.app.internalPlugins.plugins['daily-notes'].instance.options.folder;
-		return this.dailyNotesDirectory;
+	isDailyNotesEnabled() {
+    const dailyNotesPlugin = this.app.internalPlugins.plugins['daily-notes'];
+		const dailyNotesEnabled = dailyNotesPlugin && dailyNotesPlugin.enabled;
+
+		const periodicNotesPlugin = this.app.plugins.getPlugin("periodic-notes");
+		const periodicNotesEnabled = periodicNotesPlugin && periodicNotesPlugin.settings?.daily?.enabled;
+
+    return dailyNotesEnabled || periodicNotesEnabled;
 	}
 
 	getLastDailyNote() {
-		const dailyNotesDirectory = this.getDailyNotesDirectory();
-		
+		const { folder } = getDailyNoteSettings();
+
 		const files = this.app.vault.getAllLoadedFiles()
-			.filter(file => file.path.startsWith(dailyNotesDirectory))
+			.filter(file => file.path.startsWith(folder))
 			.filter(file => file.basename != null)
 			.sort((a, b) => new Date(b.basename).getTime() - new Date(a.basename).getTime());
 
@@ -32,46 +43,122 @@ export default class RolloverTodosPlugin extends Plugin {
 		const contents = await this.app.vault.read(file);
 		const unfinishedTodosRegex = /\t*- \[ \].*/g
 		const unfinishedTodos = Array.from(contents.matchAll(unfinishedTodosRegex)).map(([todo]) => todo)
+
 		return unfinishedTodos;
 	}
 
-	async onload() {
-		this.settings = await this.loadData() || { templateHeading: 'none' };
+	async rollover(file = undefined) {
+		/*** First we check if the file created is actually a valid daily note ***/
+		const { folder, format } = getDailyNoteSettings()
+		let ignoreCreationTime = false
 
-		if (!this.checkDailyNotesEnabled()) {
-			new Notice('Daily notes plugin is not enabled. Enable it and then reload Obsidian.', 2000)
+		// Rollover can be called, but we need to get the daily file
+		if (file == undefined) {
+			const allDailyNotes = getAllDailyNotes()
+			file = getDailyNote(window.moment(), allDailyNotes)
+			ignoreCreationTime = true
 		}
+		if (!file) return;
+
+		// is a daily note
+		if (!file.path.startsWith(folder)) return;
+
+		// is today's daily note
+		const today = new Date();
+		const todayFormatted = window.moment(today).format(format);
+		if (todayFormatted !== file.basename) return;
+
+		// was just created
+		if ((today.getTime() - file.stat.ctime > MAX_TIME_SINCE_CREATION) && !ignoreCreationTime) return;
+
+		/*** Next, if it is a valid daily note, but we don't have daily notes enabled, we must alert the user ***/
+		if (!this.isDailyNotesEnabled()) {
+			new Notice('RolloverTodosPlugin unable to rollover unfinished todos: Please enable Daily Notes, or Periodic Notes (with daily notes enabled).', 10000)
+		} else {
+			const { templateHeading, deleteOnComplete, removeEmptyTodos } = this.settings;
+
+			// check if there is a daily note from yesterday
+			const lastDailyNote = this.getLastDailyNote();
+			if (lastDailyNote == null) return;
+
+			// get unfinished todos from yesterday, if exist
+			let todos_yesterday = await this.getAllUnfinishedTodos(lastDailyNote)
+			if (todos_yesterday.length == 0) return;
+
+			// Potentially filter todos from yesterday for today
+			let todosAdded = 0
+			let emptiesToNotAddToTomorrow = 0
+			let todos_today = !removeEmptyTodos ? todos_yesterday : []
+			if (removeEmptyTodos) {
+				todos_yesterday.forEach((line, i)=>{
+					const trimmedLine = (line || "").trim()
+					if ((trimmedLine != '- [ ]') && (trimmedLine != '- [  ]')) {
+						todos_today.push(line)
+						todosAdded++
+					} else {
+						emptiesToNotAddToTomorrow++
+					}
+				})
+			} else {
+				todosAdded = todos_yesterday.length
+			}
+
+			// get today's content and modify it
+			if (todos_today.length > 0) {
+				let dailyNoteContent = await this.app.vault.read(file)
+				const todos_todayString = `\n${todos_today.join('\n')}`
+				if (templateHeading !== 'none') {
+					dailyNoteContent = dailyNoteContent.replace(templateHeading, `${templateHeading}${todos_todayString}`)
+				} else {
+					dailyNoteContent += todos_todayString
+				}
+				await this.app.vault.modify(file, dailyNoteContent);
+			}
+
+			// if deleteOnComplete, get yesterday's content and modify it
+			if (deleteOnComplete) {
+				let lastDailyNoteContent = await this.app.vault.read(lastDailyNote)
+				let lines = lastDailyNoteContent.split('\n')
+
+				for (let i = lines.length; i >= 0; i--) {
+					if (todos_yesterday.includes(lines[i])) {
+						lines.splice(i, 1)
+					}
+				}
+
+				const modifiedContent = lines.join('\n')
+				await this.app.vault.modify(lastDailyNote, modifiedContent);
+			}
+
+			// Let user know rollover has been successful with X todos
+			const todosAddedString = todosAdded == 0 ? "" : `- ${todosAdded} todo${todosAdded > 1 ? 's' : ''} rolled over.`
+			const emptiesToNotAddToTomorrowString = emptiesToNotAddToTomorrow == 0 ? "" :
+				(deleteOnComplete ? `- ${emptiesToNotAddToTomorrow} empty todo${emptiesToNotAddToTomorrow > 1 ? 's':''} removed.` : '')
+			const part1 = `${todosAddedString}${todosAddedString.length > 0 ? " " : ""}`
+			const part2 = `${emptiesToNotAddToTomorrowString}${emptiesToNotAddToTomorrowString.length > 0 ? " " : ""}`
+
+			const message = `${part1}${((part1.length>0) && (part2.length>0)) ? "\n" : ""}${part2}`
+			if (message.length > 0) {
+				new Notice(message, 6000)
+			}
+		}
+
+	}
+
+	async onload() {
+		await this.loadSettings()
 
 		this.addSettingTab(new RollverTodosSettings(this.app, this))
 
 		this.registerEvent(this.app.vault.on('create', async (file) => {
-			// is a daily note
-			const dailyNotesDirectory = this.getDailyNotesDirectory()
-			if (!file.path.startsWith(dailyNotesDirectory)) return;
-
-			// is today's daily note
-			const today = new Date();
-			if (getISOFormattedDate(today) !== file.basename) return;
-
-			// was just created
-			if (today.getTime() - file.stat.ctime > MAX_TIME_SINCE_CREATION) return;
-
-			const lastDailyNote = this.getLastDailyNote();
-			if (lastDailyNote == null) return;
-
-			const unfinishedTodos = await this.getAllUnfinishedTodos(lastDailyNote)
-			
-			let dailyNoteContent = await this.app.vault.read(file)
-
-			if (this.settings.templateHeading !== 'none') {
-				const heading = this.settings.templateHeading;
-				dailyNoteContent = dailyNoteContent.replace(heading, heading + '\n' + unfinishedTodos.join('\n') + '\n')
-			} else {
-				dailyNoteContent += '\n' + unfinishedTodos.join('\n')
-			}
-
-			await this.app.vault.modify(file, dailyNoteContent);
+			this.rollover(file)
 		}))
+
+		this.addCommand({
+      id: "obsidian-rollover-daily-todos",
+      name: "Rollover Todos Now",
+      callback: ()=> this.rollover()
+    })
 	}
 }
 
@@ -82,9 +169,9 @@ class RollverTodosSettings extends PluginSettingTab {
 	}
 
 	async getTemplateHeadings() {
-		const template = this.app.internalPlugins.plugins['daily-notes'].instance.options.template;
+		const { template } = getDailyNoteSettings()
 		if (!template) return [];
-		
+
 		const file = this.app.vault.getAbstractFileByPath(template + '.md')
 		const templateContents = await this.app.vault.read(file)
 		const allHeadings = Array.from(templateContents.matchAll(/#{1,} .*/g)).map(([heading]) => heading)
@@ -104,22 +191,36 @@ class RollverTodosSettings extends PluginSettingTab {
 						acc[heading] = heading;
 						return acc;
 					}, {}),
-					'none': 'None' 
+					'none': 'None'
 				})
 				.setValue(this.plugin?.settings.templateHeading)
 				.onChange(value => {
 					this.plugin.settings.templateHeading = value;
-					this.plugin.saveData(this.plugin.settings)
+					this.plugin.saveSettings();
 				})
 			)
-		}
-}
 
-/**
- * Return an ISO formatted date only for the users current timezone.
- */
-function getISOFormattedDate(date) {
-	const month = `${date.getMonth() + 1}`.padStart(2, "0")
-	const day = `${date.getDate()}`.padStart(2, "0");
-	return date.getFullYear() + "-" + month + "-" + day;
+		new Setting(this.containerEl)
+			.setName('Delete todos from previous day')
+			.setDesc(`Once todos are found, they are added to Today's Daily Note. If successful, they are deleted from Yesterday's Daily note. Enabling this is destructive and may result in lost data. Keeping this disabled will simply duplicate them from yesterday's note and place them in the appropriate section. Note that currently, duplicate todos will be deleted regardless of what heading they are in, and which heading you choose from above.`)
+			.addToggle(toggle=>toggle
+				.setValue(this.plugin.settings.deleteOnComplete || false)
+				.onChange(value=>{
+					this.plugin.settings.deleteOnComplete = value;
+					this.plugin.saveSettings();
+				})
+			)
+
+		new Setting(this.containerEl)
+			.setName('Remove empty todos in rollover')
+			.setDesc(`If you have empty todos, they will not be rolled over to the next day.`)
+			.addToggle(toggle=>toggle
+				.setValue(this.plugin.settings.removeEmptyTodos || false)
+				.onChange(value=>{
+					this.plugin.settings.removeEmptyTodos = value;
+					this.plugin.saveSettings();
+				})
+			)
+
+	}
 }
